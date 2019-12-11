5bb17be8e33e5283659502ac6d305497213fb123
melanie caro <melanie.caro@stackerine.com>
2019-12-11 15:05:40 +0100

    add a command to use twitter api

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
-const userRouter = require('./apiUser');
-const movieRouter = require('./apiMovie');
+// const userRouter = require('./apiUser');
+// const movieRouter = require('./apiMovie');
+// const axios = require('axios');
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
+
+const Twitter = require('twitter');
+const auth = require('./auth');
+
+const client = new Twitter(auth);
+
+const params = { screen_name: 'mel_caro78' };
+
+client.get('/users/show', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+const stream = client.stream('statuses/filter', { track: 'mel_caro78' });
+
+stream.on('data', function(event) {
+  client.post('statuses/filter', { status: 'Thanks!' + event.text });
+  console.log(event);
+});
 
 app.use(express.json());
 
